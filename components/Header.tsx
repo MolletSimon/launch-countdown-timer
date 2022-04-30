1c44008e4bdd90e657f4e8be360b583bf3ee9a70
Simon Mollet <mollet.simon.pro@gmail.com>
2022-05-01 00:37:54 +0200

    init project and add header

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+    return(<div>
+        <h1 className="font-redhat 
+        text-white text-3xl mt-1 
+        font-bold tracking-widest">WE'RE LAUNCHING SOON !</h1>
+    </div>);
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'bg-stars': "url('/images/hero-pattern.svg')",
+      },
+      animation: {
+        flip: 'flip 1s cubic-bezier(0, 0, 0.2, 1) infinite',
+      },
+      keyframes: {
+        flip: {
+          from: { transform: 'rotateX(0deg)', transformOrigin: '50% bottom ' },
+          to: { transform: 'rotateX(180deg)', transformOrigin: '50% bottom ' },
+        },
+      },
+    },
+    fontFamily: {
+      redhat: ['Red Hat Text', 'sans-serif'],
+    },
+    plugins: [],
   },
-  plugins: [],
 }
