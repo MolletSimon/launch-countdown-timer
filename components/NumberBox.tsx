efda02ae76c0754cfcbf437d6eca0f5a7b41b30b
Simon Mollet <mollet.simon.pro@gmail.com>
2022-05-03 23:53:45 +0200

    add mobile version and demo screenshot

--- components/NumberBox.tsx
+++ components/NumberBox.tsx
@@ -9,15 +9,16 @@ export const NumberBox = ({ unit, value }: numberBoxProps) => {
   return (
     <div>
       <div
-        className={`flex h-40 w-40 items-center justify-center 
-            rounded-xl bg-gradient-to-b from-[#2C2D45] to-[#34364F]`}
+        className={`flex h-16 w-16 items-center justify-center
+        rounded-xl bg-gradient-to-b 
+            from-[#2C2D45] to-[#34364F] md:h-40 md:w-40`}
       >
-        <span className="text-6xl font-extrabold text-[#FB6087]">
+        <span className="text-2xl font-extrabold text-[#FB6087] md:text-6xl">
           {value < 10 ? '0' + value : value}
         </span>
       </div>
       <div className="mt-3">
-        <h2 className="text-center text-sm font-bold uppercase tracking-widest text-gray-600">
+        <h2 className="text-center text-sm font-bold uppercase text-gray-600 md:tracking-widest">
           {unit}
         </h2>
       </div>
